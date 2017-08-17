4debe33c30b9a6bc487ccf047c09dcb5ce97e566
cihanbilge <kayasa89@math.uh.edu>
2017-08-17 02:34:10 -0500

    Update Script.m

--- Script.m
+++ Script.m
@@ -9,6 +9,7 @@ load('inputSoma');
 %not needed to be changed, same rectangles can be used for each of the images in dataset
 %if it is not required to create rectangles again set option.rect=0 and
 %option.cell=cell_rect;
-option.rect=0; option.cell=cell_rect;
+option.rect=1;
+%option.rect=0; option.cell=cell_rect;
 [neuriteGraph,cell_rect]=runCenterLineParallel(inputSeg,inputSoma,option);
 
