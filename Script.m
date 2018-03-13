3b0220d6e3e381ec0459f012ac24f41ac13638d8
cihanbilge <kayasa89@math.uh.edu>
2018-03-13 18:35:53 -0500

    Update Script.m

--- Script.m
+++ Script.m
@@ -5,12 +5,11 @@ clear all
 %load segmented binary image and segmented soma mask
 load('inputSeg');
 load('inputSoma');
-%cell_rect is pre-generated rectangles for seed search. For parameters are
-%not needed to be changed, same rectangles can be used for each of the images in dataset
-%if it is not required to create rectangles again set option.rect=0 and
-%option.cell=cell_rect;
-option.rect=0; option.cell=cell_rect;
-%option.rect=1;
+%cell_rect is pre-generated rectangles for seed search, which is in the folder. For the first run you need to create seed search rectangles. In that case you need to set: 
+%option.rect=1, 
+%However if you already created the rectangles and saved them with name "cell_rect", then you do not need to create rectangles again, that will help to reduce computational cost
+% In that case set: option.rect=0 and option.cell=cell_rect;
+option.rect=1;
 %to allow manual intervention: option.manual=1; 
 %to make it fully automated: option.manual=0;
 option.manual=1;
