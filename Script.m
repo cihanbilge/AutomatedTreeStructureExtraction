5fd3ab7f01e712a7dd43382a1224e6691970789e
cihanbilge <kayasa89@math.uh.edu>
2018-03-13 18:41:12 -0500

    Update Script.m

--- Script.m
+++ Script.m
@@ -6,10 +6,10 @@ clear all
 load('inputSeg');
 load('inputSoma');
 %
-%For the first run you need to create seed search rectangles. In that case you need to set: 
+%For the first run you need to create "seed search rectangles". In that case you need to set: 
 %option.rect=1, 
 %
-%However if you already created the rectangles and saved them with name "cell_rect", 
+%However, if you already created the rectangles and saved them with name "cell_rect", 
 %then you do not need to create rectangles again, that will help to reduce computational cost
 % In that case set: option.rect=0 and option.cell=cell_rect;
 %
